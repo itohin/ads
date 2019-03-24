aaef52c77bbf46f2608b87750445de8b15094987
itohin <itoxa@MacBook-Pro.local>
2019-03-24 12:39:53 +0300

    Listing model

---
+++ app/Listing.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Traits\Eloquent\OrderableTrait;
+
+class Listing extends Model
+{
+    use SoftDeletes, OrderableTrait ;
+
+    public function scopeIsLive($query)
+    {
+        return $query->where('live', true);
+    }
+
+    public function scopeIsNotLive($query)
+    {
+        return $query->where('live', false);
+    }
+
+    public function scopeFromCategory($query, Category $category)
+    {
+        return $query->where('category_id', $category->id);
+    }
+
+    public function scopeFromRegion($query, Region $region)
+    {
+        return $query->whereIn('region_id', array_merge([$region->id], $region->descendants()->pluck('id')->toArray()));
+    }
+
+    public function live()
+    {
+        return $this->live;
+    }
+
+    public function cost()
+    {
+        return $this->category->price;
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function region()
+    {
+        return $this->belongsTo(Region::class);
+    }
+}
